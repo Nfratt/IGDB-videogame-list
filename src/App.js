e9977863f7292c8c9e444fedb030050fc8793be1
Nick Fratt <nafratt@gmail.com>
2021-08-27 11:06:32 -0400

    added search

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import {Welcome} from './components/helloworld'
 
+import './App.css';
+import {SearchBar} from './components/search';
+import BookData from "./Data.json";
 
 function App() {
   return (
    <div className="App">
      <header className="App-header">
-     <img src={logo} className="App-logo" alt="logo" />
-      <Welcome name='Sara'/>
-      <Welcome name='Melissa'/>
-      <Welcome name='George'/>
-      <img src={logo} className="App-logo" alt="logo" />
+     <SearchBar placeholder="Search for a Game" data={BookData} />
       </header>
     </div>
   ); 
